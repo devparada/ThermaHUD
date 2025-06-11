3e8ae829e48bbed9857680699d8f60f16326d391
Raúl <raul.parada040103@gmail.com>
2025-06-11 18:35:46 +0200

    Mejorado el script, ahora solicita privilegios

--- start.bat
+++ start.bat
@@ -1,3 +1,14 @@
+@echo off
+title ThermaHUD
+:: —————— Comprueba los privilegios ——————
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Solicitando privilegios de administrador
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+:: —————— Si ya se ejecuta como admin ——————
 cd /d "%~dp0"
 python main.py
 pause
